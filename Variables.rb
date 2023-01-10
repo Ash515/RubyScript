73b9e2673037eeb7469db825c784eba2d60eb33e
Ashwin Kumar Ramaswamy <aaashwin515@gmail.com>
2023-01-10 19:49:06 +0530

    Added Variables.rb

---
+++ Variables.rb
@@ -0,0 +1,58 @@
+# Class variables
+
+class States
+    @@no_of_states=0
+
+    def initialize(name)
+        @states_name=name  #Instance Variables
+        @@no_of_states+=1
+
+    end
+    def display()
+        puts "States name #@states_name"
+    end
+
+    def total_no_of_states()
+        puts "Total number of states written: #@@no_of_states"
+    
+    end
+end
+
+# Object Creation
+
+first = States.new("Tamil Nadu")
+second=States.new("Kerala")
+third=States.new("Andra pradesh")
+
+#Method calling
+first.total_no_of_states()
+first.display()
+
+
+
+# Global Variables
+
+$global_var ="Ashwin"
+
+class One
+    def display
+        puts "Global variable in one is #$global_var"
+    end
+
+end
+
+class Two
+    def display
+        puts "Global vartaiable of Two is #$global_var"
+    end
+
+end
+
+
+oneobj=One.new
+oneobj.display
+
+twoobj=Two.new
+twoobj.display
+
+
